a13c7133e5c7a2d852ac80f6a488ab61696017c3
twfraley <twfraley@gmail.com>
2019-09-07 22:51:57 -0400

    Spooling out unit testing

---
+++ jobsite/tests/test_forms.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class JobFormTestCase(TestCase):
+    pass
+
+
+class ApplicationFormTestCase(TestCase):
+    pass
+
+
+class UserFormTestCase(TestCase):
+    pass
---
+++ jobsite/tests/test_views.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+
+class JobListViewTestCase(TestCase):
+    pass
